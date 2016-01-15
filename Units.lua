30cc741b76019aa14d22033b48428bc8f5d2c12d
Narthorn <narthorn@gmail.com>
2016-01-15 01:22:23 +0100

    Initial commit

---
+++ Units.lua
@@ -0,0 +1,16 @@
+Units = setmetatable({}, {__call = function(units, strName)
+	local found = {}
+	for id, unit in pairs(units) do
+		if unit and unit:GetName():lower():find(strName:lower()) then
+			found[id] = unit
+		end
+	end
+	return found
+end})
+
+local Units = Units
+local Event = Apollo.RegisterEventHandler
+Event("UnitCreated",   "f", {f = function(self, unit) Units[unit:GetId()] = unit end})
+Event("UnitDestroyed", "f", {f = function(self, unit) Units[unit:GetId()] = nil  end})
+
+Apollo.RegisterAddon(Units)
